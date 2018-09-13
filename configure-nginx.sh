8c69a6e0bc2d5d53f34b9c0be8a6f40107f37264
Thomas Petchel <tpetchel@gmail.com>
2018-09-13 12:48:27 -0700

    Initial script

---
+++ configure-nginx.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+# Update apt cache.
+sudo apt-get update
+
+# Install Nginx.
+sudo apt-get install -y nginx
+
+# Set the home page.
+echo "<html><body><h2>Welcome to Azure! My name is $(hostname).</h2></body></html>" | sudo tee -a /var/www/html/index.html
