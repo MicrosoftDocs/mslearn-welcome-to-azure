1ec527efc319f0e466bb6e8eeed98f5682e89bb5
vishalvish83github <vishalvish83@gmail.com>
2019-01-11 04:37:02 +0530

    Update configure-nginx.sh

--- configure-nginx.sh
+++ configure-nginx.sh
@@ -7,4 +7,6 @@ sudo apt-get update
 sudo apt-get install -y nginx
 
 # Set the home page.
+echo "<html><body><h2>Welcome to Azure! My name is Vishal Vishwakarma.</h2></body></html>" | sudo tee -a /var/www/html/index.html
+echo "<html><body><h2>This is my own Virtual Machine WebServer's First Web page</h2></body></html>" | sudo tee -a /var/www/html/index.html
 echo "<html><body><h2>Welcome to Azure! My name is $(hostname).</h2></body></html>" | sudo tee -a /var/www/html/index.html
