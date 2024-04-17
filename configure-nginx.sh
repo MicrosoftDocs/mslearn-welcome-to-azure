f87e62655d9a9183e95a73501125b289d2ef62d5
Rob-Barefoot <robbarefoot@microsoft.com>
2024-04-17 09:57:22 -0400

    Update configure-nginx.sh
    removed -core flag from nginx install

--- configure-nginx.sh
+++ configure-nginx.sh
@@ -4,7 +4,7 @@
 sudo apt-get update
 
 # Install Nginx.
-sudo apt-get install -y nginx-core
+sudo apt-get install -y nginx
 
 # Set the home page.
 echo "<html><body><h2>Welcome to Azure! My name is $(hostname).</h2></body></html>" | sudo tee -a /var/www/html/index.html
